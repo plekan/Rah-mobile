fc66cb560c143f620fc56b47455994c47a24ae09
Oleksandr Zelentsov <saze1997@gmail.com>
2019-04-11 16:39:34 +0200

    changed activity name, added labels, changed the layout of register activity

--- app/src/main/java/com/example/rah/LoginActivity.java
+++ app/src/main/java/com/example/rah/LoginActivity.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         buttonLogin = findViewById(R.id.buttonLogin);
 
     }
